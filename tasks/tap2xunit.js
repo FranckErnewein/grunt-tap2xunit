d2c99b4980c26c902c2e7d1cdfac3329b542e341
Franck Ernewein <franck.ernewein@gmail.com>
2014-03-04 18:49:32 +0100

    temp function

--- tasks/tap2xunit.js
+++ tasks/tap2xunit.js
@@ -7,6 +7,73 @@
  */
 
 'use strict';
+  function tap2xml(){
+      var tapString = grunt.file.read( TESTEM_RESULT_FILE, { encoding: 'utf8' });
+      var lines = tapString.split('\n');
+
+      function TapLine( str ){
+        this.string = str;
+      }
+
+      TapLine.prototype = {
+        getLabel: function( line ){
+          var label =  this.string.split( ' - ' )[1];
+          var escaped = label.replace(/&/g, '&amp;')
+                             .replace(/</g, '&lt;')
+                             .replace(/>/g, '&gt;')
+                             .replace(/"/g, '&quot;')
+                             .replace(/'/g, '&apos;');
+          return escaped;
+        },
+        getInteger: function(){
+          return this.string.replace( /^\D+/g, '');
+        },
+        isComment: function(){
+          return this.string.charAt(0) === '#';
+        },
+        hasContent: function( indexStart, indexEnd, content ){
+            return this.string.slice( indexStart, indexEnd ) === content;
+        }
+      };
+
+      var xml = '',
+          now = new Date().toISOString(),
+          tests = [],
+          nbTest,
+          nbTestPassed,
+          nbTestFailed;
+      
+
+      for( var i=0; i<lines.length; i++ ){ 
+          var line = new TapLine( lines[i] );
+          if( line.isComment() ){
+              if( line.hasContent( 2, 7, 'tests' ) ){
+                nbTest = line.getInteger();
+              /*
+              }else if( line.hasContent( 2, 6, 'pass' ) ){
+                nbTestPassed = line.getInteger();
+              */
+              }else if( line.hasContent( 2, 6, 'fail' ) ){
+                nbTestFailed = line.getInteger();
+              }
+          }else if( line.hasContent( 0, 2, 'ok' ) ){
+            tests.push('<testcase name="'+line.getLabel()+'"></testcase>');
+          }else if( line.hasContent( 0, 6, 'not ok' ) ){
+            tests.push('<testcase name="'+line.getLabel()+'"><failure>Test fail</failure></testcase>');
+          }
+      }
+
+      xml += '<testsuites>';
+      xml += '<testsuite name="Javascript unit test with Testem" tests="'+nbTest+'" failures="'+nbTestFailed+'" errors="0" timestamp="'+ now +'">';
+      xml += tests.join('');
+      xml += '</testsuite>';
+      xml += '</testsuites>';
+
+      grunt.file.write('build/logs/' + TESTEM_RESULT_FILE.replace('.tap','.xml'), xml );
+      //delete temp file
+      grunt.file.delete( TESTEM_RESULT_FILE );
+       
+  }
 
 module.exports = function(grunt) {
 
