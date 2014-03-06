501419c3217ace16ab0354628f284f35ff0e567d
Franck Ernewein <franck.ernewein@gmail.com>
2014-03-06 08:34:13 +0100

    isolate TapLine Class

--- tasks/tap2xunit.js
+++ tasks/tap2xunit.js
@@ -7,34 +7,37 @@
  */
 
 'use strict';
+
+function TapLine( str ){
+  this.string = str;
+}
+
+TapLine.prototype = {
+  getLabel: function( line ){
+    var label =  this.string.split( ' - ' )[1];
+    var escaped = label.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
+    return escaped;
+  },
+  getInteger: function(){
+    return this.string.replace( /^\D+/g, '');
+  },
+  isComment: function(){
+    return this.string.charAt(0) === '#';
+  },
+  hasContent: function( indexStart, indexEnd, content ){
+    return this.string.slice( indexStart, indexEnd ) === content;
+  }
+};
+
+/*
   function tap2xml(){
       var tapString = grunt.file.read( TESTEM_RESULT_FILE, { encoding: 'utf8' });
       var lines = tapString.split('\n');
 
-      function TapLine( str ){
-        this.string = str;
-      }
-
-      TapLine.prototype = {
-        getLabel: function( line ){
-          var label =  this.string.split( ' - ' )[1];
-          var escaped = label.replace(/&/g, '&amp;')
-                             .replace(/</g, '&lt;')
-                             .replace(/>/g, '&gt;')
-                             .replace(/"/g, '&quot;')
-                             .replace(/'/g, '&apos;');
-          return escaped;
-        },
-        getInteger: function(){
-          return this.string.replace( /^\D+/g, '');
-        },
-        isComment: function(){
-          return this.string.charAt(0) === '#';
-        },
-        hasContent: function( indexStart, indexEnd, content ){
-            return this.string.slice( indexStart, indexEnd ) === content;
-        }
-      };
 
       var xml = '',
           now = new Date().toISOString(),
@@ -49,10 +52,6 @@
           if( line.isComment() ){
               if( line.hasContent( 2, 7, 'tests' ) ){
                 nbTest = line.getInteger();
-              /*
-              }else if( line.hasContent( 2, 6, 'pass' ) ){
-                nbTestPassed = line.getInteger();
-              */
               }else if( line.hasContent( 2, 6, 'fail' ) ){
                 nbTestFailed = line.getInteger();
               }
@@ -74,6 +73,7 @@
       grunt.file.delete( TESTEM_RESULT_FILE );
        
   }
+  */
 
 module.exports = function(grunt) {
 
@@ -81,36 +81,10 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('tap2xunit', 'Convert tap file into xml file', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    this.files.forEach( function( src, dest ) {
+      grunt.log.writeln('File "' + dest + '" created.');
     });
   });
 
